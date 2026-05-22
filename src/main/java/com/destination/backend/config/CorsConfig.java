a4cfda77b6ea4950c9772445e5655edbbf175145
Nithin raj <nithinraj1503@gmail.com>
2026-05-22 21:57:24 +0530

    fix

--- src/main/java/com/destination/backend/config/CorsConfig.java
+++ src/main/java/com/destination/backend/config/CorsConfig.java
@@ -1,20 +1,29 @@
 package com.destination.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
 
-        registry.addMapping("/**")
-                //.allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://destinationworld.vercel.app")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+        return new WebMvcConfigurer() {
 
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://destinationworld.vercel.app")
+                        // .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
