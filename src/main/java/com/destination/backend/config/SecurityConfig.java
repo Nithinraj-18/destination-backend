d5e89beb439bc0d680fdc4956ce3363eaf0f6f3a
Nithin raj <nithinraj1503@gmail.com>
2026-05-22 22:23:20 +0530

    confix path fix

--- src/main/java/com/destination/backend/config/SecurityConfig.java
+++ src/main/java/com/destination/backend/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://destinationworld.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
